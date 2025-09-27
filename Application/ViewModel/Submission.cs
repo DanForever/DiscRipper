927accace224280a6e52c75444468acb06f57f8e
Dan Neve <6909667+DanForever@users.noreply.github.com>
2025-09-27 21:08:10 +0200

    Added a region code selector

--- Application/ViewModel/Submission.cs
+++ Application/ViewModel/Submission.cs
@@ -26,8 +26,18 @@ internal class Submission : ViewModel
     public string? EditionName { get; set; }
     public string? DiscTitle { get; set; }
     public string? DiscSlug { get; set; }
-    public TheDiscDb.RegionCode RegionCode { get; set; }
-    public string? Locale { get; set; }
+
+    public TheDiscDb.RegionCode RegionCode
+    {
+        get => Model.RegionCode;
+        set => ChangeProperty(Model, value);
+    }
+
+    public string? Locale
+    {
+        get => Model.Locale;
+        set => ChangeProperty(Model, value);
+    }
 
     public required IEnumerable<SubmissionTitle> Titles { get; init; }
 }
