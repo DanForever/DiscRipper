c52ad54cd046b3966a42b8251c761998999d5eec
Dan Neve <6909667+DanForever@users.noreply.github.com>
2025-12-07 13:25:45 +0100

    refactor: SubmitNewDisc now uses common usercontrols
    Also fixed missing event propagation from media type property

--- Application/Controls/ReleaseProperties.xaml.cs
+++ Application/Controls/ReleaseProperties.xaml.cs
@@ -1,28 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace DiscRipper.Controls
 {
-	/// <summary>
-	/// Interaction logic for ReleaseProperties.xaml
-	/// </summary>
 	public partial class ReleaseProperties : UserControl
 	{
 		public ReleaseProperties()
 		{
 			InitializeComponent();
+		}
+
+		public event System.Windows.Controls.SelectionChangedEventHandler? MediaTypeChanged;
+
+		private void OnMediaTypeChanged(object sender, SelectionChangedEventArgs e)
+		{
+			MediaTypeChanged?.Invoke(sender, e);
 		}
 	}
 }
