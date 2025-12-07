6f8fec4ea4df43a71c17331ae5e5faa75595a1ea
Dan Neve <6909667+DanForever@users.noreply.github.com>
2025-12-07 13:49:32 +0100

    refactor: Unified the SubmitNewDisc constructors

--- Application/Windows/SubmitNewDisc.xaml.cs
+++ Application/Windows/SubmitNewDisc.xaml.cs
@@ -31,49 +31,6 @@ namespace DiscRipper
 			AllRegions = CultureInfo.GetCultures(CultureTypes.SpecificCultures).OrderBy(c => c.DisplayName);
 		}
 
-		public SubmitNewDisc(MakeMkv.Log log, List<Types.Title> titles, MakeMkv.Drive? drive = null)
-		{
-			InitializeComponent();
-
-			IList<SubmissionTitle> submissionTitles = titles.Select(t => new SubmissionTitle { Model = t }).ToList();
-
-			Log = log;
-			Drive = drive;
-
-			Session = SessionManager.Instance.Value.CreateSession(titles);
-			Session.Log = Log.ExportRawLog();
-
-			Submission = new()
-			{
-				DiscModel = Session.Disc,
-				Model = Session.Release,
-				Titles = submissionTitles,
-
-				// sensible defaults
-				Locale = "en-US",
-				MediaType = MediaTypes[0],
-			};
-
-			DataContext = Submission;
-
-			if (Drive is null)
-				return;
-
-			switch (Drive.DiscType)
-			{
-			case MakeMkv.DiscType.DVD:
-				Submission.DiscFormat = "DVD";
-				break;
-
-			case MakeMkv.DiscType.BD:
-			default:
-				Submission.DiscFormat = "Blu-Ray";
-				break;
-			}
-
-			Loaded += async (_, _) => await GenerateHashData();
-		}
-
 		public SubmitNewDisc(Session session, MakeMkv.Log log)
 		{
 			InitializeComponent();
@@ -91,6 +48,33 @@ namespace DiscRipper
 			};
 
 			DataContext = Submission;
+		}
+
+		public SubmitNewDisc(MakeMkv.Log log, List<Types.Title> titles, MakeMkv.Drive? drive = null) : this(SessionManager.Instance.Value.CreateSession(titles), log)
+		{
+			Session.Log = Log.ExportRawLog();
+
+			// sensible defaults
+			Submission.Locale = CultureInfo.CurrentCulture.Name;
+			Submission.MediaType = MediaTypes[0];
+
+			Drive = drive;
+			if (Drive is null)
+				return;
+
+			switch (Drive.DiscType)
+			{
+			case MakeMkv.DiscType.DVD:
+				Submission.DiscFormat = "DVD";
+				break;
+
+			case MakeMkv.DiscType.BD:
+			default:
+				Submission.DiscFormat = "Blu-Ray";
+				break;
+			}
+
+			Loaded += async (_, _) => await GenerateHashData();
 		}
 
 		private async Task<TheDiscDb.Submit.SubmissionContext> RunSteps(IList<TheDiscDb.Submit.IStep> steps)
@@ -160,7 +144,7 @@ namespace DiscRipper
 
 		private void Cancel_Click(object sender, RoutedEventArgs e)
 		{
-
+			Close();
 		}
 
 		private void MediaType_Changed(object sender, SelectionChangedEventArgs e)
