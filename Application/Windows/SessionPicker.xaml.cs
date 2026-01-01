e5cf0273a59c51e371f76d39971aa6178624c1b9
Dan Neve <6909667+DanForever@users.noreply.github.com>
2026-01-01 18:33:37 +0100

    Updated the session picker to wpfui

--- Application/Windows/SessionPicker.xaml.cs
+++ Application/Windows/SessionPicker.xaml.cs
@@ -4,38 +4,37 @@ using System.Windows.Input;
 
 using DiscRipper.Sessions;
 
-namespace DiscRipper.Windows
+namespace DiscRipper.Windows;
+
+internal partial class SessionPicker
 {
-	public partial class SessionPicker : Window
+	public SessionPicker()
 	{
-		public SessionPicker()
-		{
-			InitializeComponent();
+		InitializeComponent();
 
-			DataContext = SessionManager.Instance.Value.SessionList;
-		}
+		DataContext = SessionManager.Instance.Value.SessionList;
+	}
 
-		private async void Session_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+	private async void Session_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+	{
+		var row = ItemsControl.ContainerFromElement((DataGrid)sender, e.OriginalSource as DependencyObject) as DataGridRow;
+		if (row != null && row.Item is Session session)
 		{
-			var row = ItemsControl.ContainerFromElement((DataGrid)sender, e.OriginalSource as DependencyObject) as DataGridRow;
-			if (row != null && row.Item is Session session)
+			MakeMkv.Log? log = await SessionManager.Instance.Value.LoadLog(session);
+
+			if (log is null)
+				return;
+
+			if (session.Release is not null)
 			{
-				MakeMkv.Log? log = await SessionManager.Instance.Value.LoadLog(session);
-
-				if (log is null)
-					return;
-
-				if (session.Release is not null)
-				{
-					SubmitRelease submitNewDisc = new(session, log) { Owner = this };
-					submitNewDisc.Show();
-				}
-				else
-				{
-					SubmitAdditionalDisc submitSecondDisc = new(session, log) { Owner = this };
-					submitSecondDisc.Show();
-					return;
-				}
+				SubmitRelease submitNewDisc = new(session, log) { Owner = this };
+				submitNewDisc.Show();
+			}
+			else
+			{
+				SubmitAdditionalDisc submitSecondDisc = new(session, log) { Owner = this };
+				submitSecondDisc.Show();
+				return;
 			}
 		}
 	}
