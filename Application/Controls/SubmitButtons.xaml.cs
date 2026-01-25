edef53753c09e894bdb0a7e3c9d434246b02273b
Dan Neve <6909667+DanForever@users.noreply.github.com>
2026-01-25 11:34:41 +0100

    Submit additional disc updated to WPF-UI
    Consolidated the submit buttons

---
+++ Application/Controls/SubmitButtons.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DiscRipper.Controls;
+
+internal partial class SubmitButtons : UserControl
+{
+	#region C-Tor
+
+	public SubmitButtons()
+	{
+		InitializeComponent();
+	}
+
+	#endregion C-Tor
+
+	#region Events
+
+	public event RoutedEventHandler Submit
+	{
+		add => SubmitButton.Click += value;
+		remove => SubmitButton.Click -= value;
+	}
+
+	public event RoutedEventHandler Cancel
+	{
+		add => CancelButton.Click += value;
+		remove => CancelButton.Click -= value;
+	}
+
+	#endregion Events
+}
